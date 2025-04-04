f1f34d6b7f158e63a93b1146b45bbaa5b6db6bcf
whirling-in-rags <72390280+nschoenwald@users.noreply.github.com>
2025-04-04 16:02:29 +0200

    1.0.0

--- bloodDrop.js
+++
@@ -1,5 +0,0 @@
-Hooks.once("init", () => {
-  // Override the bloodied icon path
-  CONFIG.DND5E.bloodied.icon = "assets/bloodied.webp";
-  console.log("Successfully updated the Bloodied icon!");
-});
