2db02e45752f3682b59f1e2acbec541d3948c7ca
nschoenwald <72390280+nschoenwald@users.noreply.github.com>
2025-04-09 20:57:10 +0200

    Update bloodDrop.js

--- scripts/bloodDrop.js
+++ scripts/bloodDrop.js
@@ -1,5 +1,5 @@
 Hooks.once("init", () => {
   // Override the bloodied icon path
-  CONFIG.DND5E.bloodied.icon = "assets/bloodDrop.webp";
+  CONFIG.DND5E.bloodied.icon = "assets/bloodDrop.svg";
   console.log("Successfully updated the Bloodied icon!");
 });
